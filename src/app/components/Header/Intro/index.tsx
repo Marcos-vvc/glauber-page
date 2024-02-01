49e7b35977e3ff1df2400991c24f7be3127d9f06
Marcos-vvc <marcos-dido@hotmail.com>
2024-02-01 17:40:25 -0300

    foto atualizada

--- src/app/components/Header/Intro/index.tsx
+++ src/app/components/Header/Intro/index.tsx
@@ -4,7 +4,7 @@ import glauber from '../../../../../public/Glauber.png'
 export default function Intro() {
   return (
     <div className="flex flex-col text-white justify-center items-center uppercase lg:flex lg:flex-row p-8  lg:items-center lg:m-auto lg:max-w-[70rem] lg:gap-96 lg:mt-10">
-      <div className="mt-10 flex flex-col justify-center items-center lg:flex lg:flex-col lg:m-auto">
+      <div className="mt-10 flex flex-col justify-center items-center lg:flex lg:flex-col lg:m-auto lg:justify-center">
         <h2 className="font-bold text-7xl text-center">
           Glauber <br /> Aguiar
         </h2>
@@ -13,7 +13,7 @@ export default function Intro() {
         </span>
         <p className="flex text-xs mt-2">Sua melhor versão começa aqui!</p>
       </div>
-      <Image src={glauber} alt="" className="flex w-72 h-80 rounded mt-6" />
+      <Image src={glauber} alt="" className="flex w-[25rem] h-[30rem]" />
     </div>
   )
 }
