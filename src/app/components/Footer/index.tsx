454e48808ed4fb47d9819441ea7b94d47dfc208a
Marcos-vvc <marcos-dido@hotmail.com>
2024-02-05 20:25:34 -0300

    inserindo link nos contatos

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import logo from '../../../../public/Logo.png'
-import { Instagram, Phone } from 'lucide-react'
+import whats from '../../../../public/whatsapp.svg'
+import { Instagram } from 'lucide-react'
 
 export function Footer() {
   return (
@@ -14,12 +15,23 @@ export function Footer() {
       </footer>
       <div className="flex flex-col gap-4 justify-center items-center mt-8">
         <h4 className="font-bold text-3xl">Contatos</h4>
-        <span className="text-white flex gap-2">
-          <Instagram /> @euglauberaguiar
-        </span>
-        <span className="flex gap-2">
-          <Phone /> (61)986343416
-        </span>
+        <a
+          href="https://www.instagram.com/euglauberaguiar?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+          target="blank"
+        >
+          <span className="text-white flex gap-2">
+            <Instagram /> @euglauberaguiar
+          </span>
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=5561986343416&text&type=phone_number&app_absent=0"
+          target="blank"
+        >
+          <span className="flex gap-2">
+            <Image src={whats} alt="" className="w-6 h-6 text-white" />
+            (61)986343416
+          </span>
+        </a>
       </div>
     </div>
   )
