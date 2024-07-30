ecb8590beabd091d0a57eba4f040865f846856d8
Marcos-vvc <marcos-dido@hotmail.com>
2024-07-30 16:42:03 -0300

    feat: implementação de pixel.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,37 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
-import { Sidebar } from './components/Header'
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import { Sidebar } from './components/Header';
+import FacebookPixel from './components/Pixel/FacebookPixel';
+import Head from 'next/head';
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['300', '700'],
-})
+});
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Glauber Aguiar',
   description: 'Lading Page Glauber Aguiar',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html
       lang="en"
       className={`${poppins.className} scrollbar-thumb-gb-25 scrollbar-track-transparent scrollbar-thin overflow-y-scroll`}
     >
-      <link rel="icon" type="image/svg+xml" href="/Logo.png" />
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+
+        <link rel="icon" type="image/svg+xml" href="/Logo.png" />
+        <FacebookPixel />
+      </Head>
 
       <body className="bg-black ">
         <div className="min-h-screen overflow-hidden">
@@ -32,5 +40,5 @@ export default function RootLayout({
         </div>
       </body>
     </html>
-  )
+  );
 }
